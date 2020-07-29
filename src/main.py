0eec0fe64bed8d46f893088e9f7a5d05364914cd
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-07-29 00:14:56 -0700

    Add size and test size

--- src/main.py
+++ src/main.py
@@ -27,3 +27,25 @@ class Move:
         move_command = self.move_command_gen(self.x, self.y)
         subprocess.run(move_command, shell=True)
         return str(move_command)
+ 
+
+class Size:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_current(self):
+        bspc_window = "bspc query -N -n"
+        current_window = subprocess.check_output(bspc_window, shell=True)
+        return current_window.decode("utf-8").strip('\n')
+
+    def size_command_gen(self, x, y):
+        command_prefx = "xdotool windowsize "
+        command = f"{self.get_current()} {x}% {y}%"
+        return command_prefx + command
+
+    def size_win(self):
+        size_command = self.size_command_gen(self.x, self.y)
+        subprocess.run(size_command, shell=True)
+        return str(size_command)
+
